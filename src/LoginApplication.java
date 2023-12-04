9ec983f8004dec4144d1d521d8b01fd91819f1e4
besart jashari <jashari.besart@bene-edu.ch>
2023-12-04 19:53:50 +0100

    update endlos schleife

--- src/LoginApplication.java
+++ src/LoginApplication.java
@@ -2,16 +2,24 @@ import java.util.Scanner;
 
 public class LoginApplication {
     public static void main(String[] args) {
-        UserDatabase userDatabase = new UserDatabase("src/users.csv");  // Pfad anpassen
+        UserDatabase userDatabase = new UserDatabase("src/users.csv");
         LoginService loginService = new LoginService(userDatabase);
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Bitte geben Sie Ihren Namen ein:");
         String name = scanner.nextLine();
+        boolean loginSuccess = false;
 
-        System.out.println("Bitte geben Sie Ihren PIN ein:");
-        String pin = scanner.nextLine();
+        while (!loginSuccess) {
+            System.out.println("Bitte geben Sie Ihren PIN ein:");
+            String pin = scanner.nextLine();
+            loginSuccess = loginService.login(name, pin);
+            if (!loginSuccess) {
+                System.out.println("Falscher PIN. Bitte versuchen Sie es erneut.");
+            }
+        }
 
-        loginService.login(name, pin);
+        System.out.println("Anmeldung erfolgreich!");
+        scanner.close();
     }
 }
